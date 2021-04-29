b5060b39f0c8737a5bb41d43092f3e18ada2a163
Rola11 <82285110+Rola11@users.noreply.github.com>
2021-04-30 00:55:34 +0300

    Add files via upload

---
+++ Code.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+   // declare variables
+   char str[200];
+   int line, word, ch;
+
+   // initialize count variables with zero
+   line = word = ch = 0;
+
+   // read multiline string
+   printf("Enter string terminated with ~ :\n");
+   scanf("%[^~]", str);
+
+   // check every character
+   for(int i=0; str[i]!='\0'; i++)
+   {
+      // if it is new line then
+      // one line and one word completed
+      if(str[i]=='\n')
+      {
+         line++;
+      }
+
+      // else it is a character
+      else 
+      {
+         // if character is space or tab
+         // then one word is also completed
+         if(str[i]==' '||str[i]=='\t')
+         {
+            word++;
+         }
+
+         // it was not '\n', sapace or tab
+         // it is a normal character
+         else {
+            ch++;
+         }
+      }
+   }
+
+   // display count values
+   printf("\nCharacter counts = %d\n", ch);
+   printf("Word counts = %d\n", word+1);
+   printf("Line counts = %d\n", line+1);
+
+   return 0;
+}
